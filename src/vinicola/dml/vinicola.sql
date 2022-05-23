73bff797965fbb736ce1347a248dc024b012aa78
Celso Antonio Uliana Junior <32890738+CelsoUliana@users.noreply.github.com>
2022-05-23 01:29:40 -0400

    teste de insert vinho

--- src/vinicola/dml/vinicola.sql
+++ src/vinicola/dml/vinicola.sql
@@ -3,3 +3,7 @@ INSERT INTO vinicola.regiao VALUES (2, 'Tejo', 'Mapa 2 - Portugal', 'Região de 
 INSERT INTO vinicola.regiao VALUES (3, 'Vale dos Vinhedos', 'Mapa 1 - Rio Grande so Sul', 'Região de vinícolas famosa no Rio Grande do Sul');
 
 INSERT INTO vinicola.vinicola VALUES (1, 'Cave Geisse', 'Considerados os melhores espumantes da america latina', '559999999', '559912319', 3);
+INSERT INTO vinicola.vinicola VALUES (2, 'Cave Falua - Sociedade de Vinhos', ' Falua - Sociedade de Vinhos, tem produzido nas suas instalações de vinificação em Almeirim desde 2004 vinhos de grande qualidade.', '55999998', '559912318', 2);
+INSERT INTO vinicola.vinicola VALUES (3, 'Picowines', 'Instalação na região de Açores, necessário reserva.', '55999997', '559912317', 1);
+
+INSERT INTO vinicola.vinho VALUES (1, 'Conde Vimioso Sommelier', 'Tinto', 2017, 'Conde Vimioso Sommelier Edition Tinto 2017, Vinho Regional do Tejo', 90.50, 2);
